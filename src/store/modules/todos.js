9ed7db4a989e4a38bc04ef8a16387eb1b0760aa2
victor mejia <victormejia@victors-MacBook-Pro.local>
2020-08-08 15:22:33 -0600

    rendering the hard coded todos that are stored in the todos module

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,11 +1,22 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 const state = {
-
+    todos: [
+        {
+            id: 1,
+            title: 'todo one'
+        },
+        {
+            id: 2,
+            title: 'todo two'
+        }
+    ]
 }
 
 const getters = {
-
+    allTodos: (state) => {
+        return state.todos
+    }
 }
 
 const actions = {
