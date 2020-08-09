4b5b1e8f94aa8ea59ec09ea59be4c05b7eaf3646
victor mejia <victormejia@victors-MacBook-Pro.local>
2020-08-09 14:22:16 -0600

    made an action and alled a mutation that deletes a a todo based on its id

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
       <div class="todos">
           <div v-for="todo in allTodos" :key="todo.id" class="todo">
               {{todo.title}}
+              <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
           </div>
       </div>
   </div>
@@ -14,7 +15,7 @@ import { mapGetters, mapActions } from  'vuex';
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(['fetchTodos'])
+        ...mapActions(["fetchTodos", "deleteTodo"])
     },
     computed: mapGetters(['allTodos']),
     created () {
@@ -38,6 +39,14 @@ export default {
         border-radius: 5px;
         text-align: center;
         position: relative;
+        cursor: pointer;
+    }
+
+    i {
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        color: #fff;
         cursor: pointer;
     }
 
