ef0f3dd700c034815ccec8259f6a30daebf0e507
victor mejia <victormejia@victors-MacBook-Pro.local>
2020-08-09 11:13:28 -0600

    returning and rendering all todos through actions mutations getters and state

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,16 +1,9 @@
 // import axios from 'axios'
 
+import axios from "axios"
+
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'todo one'
-        },
-        {
-            id: 2,
-            title: 'todo two'
-        }
-    ]
+    todos: []
 }
 
 const getters = {
@@ -18,13 +11,16 @@ const getters = {
         return state.todos
     }
 }
-
+//action is going to make a request, get a response and call a mutation because mutation is what actually mutates the state
 const actions = {
-
+    async fetchTodos ({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        commit('setTodos', response.data)
+    }
 }
 
 const mutations = {
-
+    setTodos: (state, todos) => (state.todos = todos)
 }
 
 export default {
